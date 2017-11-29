72aec0f6a9d45f51bdd145a3cc301472543f04b0
jakkornat <jakkornat@gmail.com>
2017-11-29 08:24:35 +0100

    Adding extension and README

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+<img src="https://img.shields.io/badge/Swift-4.0-orange.svg" />
+<a href="https://twitter.com/jkornat">
+<img src="https://img.shields.io/badge/contact-@jkornat-blue.svg?style=flat" alt="Twitter: @jkornat" />
+</a>
+</p>
+
+## UIColor extension
+
+UIColor class extension.
+
+## How it's working?
+
+Creating a color based on hex format of the color, i.e. #ffffff
+
+## Autor
+
+* **Jakub Kornatowski** - iOS & MacOS Dev and Specialist
+CEO & Founder / Freelancer in Qcoder
+🇵🇱 Blogger at https://swiftly.pl
+
+Available for you on: 
+Twiter - <a href="https://twitter.com/jkornat">@jkornat</a>
+Snapchat - <a href="https://snapchat.com/add/jakornat">jakornat</a>
+Instagram - <a href="https://instagram.com/jkornat">@jkornat</a>
+
+## License
+
+GPL Licence (v3.0) http://www.gnu.org/licenses/gpl-3.0.txt
---
+++ UIColor+hex.playground/Contents.swift
@@ -0,0 +1,40 @@
+// @author Jakub Kornatowski
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(hexColor:Int) {
+        self.init(red:(hexColor >> 16) & 0xff, green:(hexColor >> 8) & 0xff, blue: hexColor & 0xff)
+    }
+    
+    static func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
+
